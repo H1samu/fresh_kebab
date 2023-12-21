1049d7e6319b24d2436bfa2532270b3e19e4597d
gadel <Khisamutdinov.g@gmail.com>
2023-12-21 23:23:48 +0400

    День #11
    - Исправление некоторых недочетов
    - Заполнение части товаров на экране Меню

--- lib/mobile/menu_mobile.dart
+++ lib/mobile/menu_mobile.dart
@@ -139,22 +139,244 @@ Widget menuMobile() {
             const SizedBox(height: 50),
             const Row(
               children: [
-                ProductCard(
+                ProductCardSmall(
                     imagePath: 'assets/images/fries/small.png',
                     textTitle: 'Малая картошка фри',
                     textWeight: 'Вес: 100гр.',
                     textPrice: '89 ₽'),
                 Spacer(),
-                ProductCard(
-                    imagePath: 'assets/images/pizza/cheese.png',
-                    textTitle: 'Пицца "Сырная"',
-                    textDescription:
-                        'Соус итальянский, фирменный соус, сыр моцарелла, сыр чеддер, сыр брынза, 2 вида фирменных специй, соус песто',
-                    textWeight: 'Вес: 560гр.',
-                    textPrice: '480 ₽'),
+                ProductCardSmall(
+                    imagePath: 'assets/images/fries/big.png',
+                    textTitle: 'Большая картошка фри',
+                    textWeight: 'Вес: 150гр.',
+                    textPrice: '129 ₽'),
               ],
             ),
             const SizedBox(height: 50),
+            const Row(
+              children: [
+                ProductCardSmall(
+                    imagePath: 'assets/images/fries/derevenski.png',
+                    textTitle: 'Картофель по-деревенски',
+                    textWeight: 'Вес: 150гр.',
+                    textPrice: '129 ₽'),
+                Spacer(),
+                ProductCardSmall(
+                    imagePath: 'assets/images/fries/bbq.png',
+                    textTitle: 'Соус барбекю',
+                    textWeight: 'Объем: 25мл.',
+                    textPrice: '35 ₽'),
+              ],
+            ),
+            const SizedBox(height: 50),
+            const Row(
+              children: [
+                ProductCardSmall(
+                    imagePath: 'assets/images/fries/cheese.png',
+                    textTitle: 'Сырный соус',
+                    textWeight: 'Объем: 25мл.',
+                    textPrice: '35 ₽'),
+                Spacer(),
+                ProductCardSmall(
+                    imagePath: 'assets/images/fries/tomato.png',
+                    textTitle: 'Соус томатный',
+                    textWeight: 'Объем: 25мл.',
+                    textPrice: '35 ₽'),
+              ],
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.only(bottom: 50, top: 30),
+              child: const Text(
+                "Напитки",
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 30),
+              ),
+            ),
+            const Row(
+              children: [
+                ProductCardSmall(
+                    imagePath: 'assets/images/drinks/cranberry.png',
+                    textTitle: 'Морс клюквенный',
+                    textWeight: 'Объем: 500мл.',
+                    textPrice: '109 ₽'),
+                Spacer(),
+                ProductCardSmall(
+                    imagePath: 'assets/images/drinks/cranberry_small.png',
+                    textTitle: 'Морс клюквенный',
+                    textWeight: 'Объем: 230мл.',
+                    textPrice: '49 ₽'),
+              ],
+            ),
+            const SizedBox(height: 50),
+            const Row(
+              children: [
+                ProductCardSmall(
+                    imagePath: 'assets/images/drinks/seabuckthorn.png',
+                    textTitle: 'Морс облепиховый',
+                    textWeight: 'Объем: 500мл.',
+                    textPrice: '109 ₽'),
+                Spacer(),
+                ProductCardSmall(
+                    imagePath: 'assets/images/drinks/seabuckthorn_small.png',
+                    textTitle: 'Морс облепиховый',
+                    textWeight: 'Объем: 230мл.',
+                    textPrice: '49 ₽'),
+              ],
+            ),
+            const SizedBox(height: 50),
+            const Row(
+              children: [
+                ProductCardSmall(
+                    imagePath: 'assets/images/drinks/currant.png',
+                    textTitle: 'Морс смородиновый',
+                    textWeight: 'Объем: 500мл.',
+                    textPrice: '109 ₽'),
+                Spacer(),
+                ProductCardSmall(
+                    imagePath: 'assets/images/drinks/currant_small.png',
+                    textTitle: 'Морс смородиновый',
+                    textWeight: 'Объем: 230мл.',
+                    textPrice: '49 ₽'),
+              ],
+            ),
+            const SizedBox(height: 50),
+            const Row(
+              children: [
+                ProductCardSmall(
+                    imagePath: 'assets/images/drinks/cranberry.png',
+                    textTitle: 'Морс клюквенный',
+                    textWeight: 'Объем: 500мл.',
+                    textPrice: '109 ₽'),
+                Spacer(),
+                ProductCardSmall(
+                    imagePath: 'assets/images/drinks/cranberry_small.png',
+                    textTitle: 'Морс клюквенный',
+                    textWeight: 'Объем: 230мл.',
+                    textPrice: '49 ₽'),
+              ],
+            ),
+            const SizedBox(height: 50),
+            const Row(
+              children: [
+                ProductCardLong(
+                    imagePath: 'assets/images/drinks/pulpy.png',
+                    textTitle: 'Pulpy',
+                    textWeight: 'Объем: 500мл.',
+                    textPrice: '109 ₽'),
+                Spacer(),
+                ProductCardLong(
+                    imagePath: 'assets/images/drinks/cola.png',
+                    textTitle: '"Добрый" Cola',
+                    textWeight: 'Объем: 500мл.',
+                    textPrice: '99 ₽'),
+              ],
+            ),
+            const SizedBox(height: 50),
+            const Row(
+              children: [
+                ProductCardLong(
+                    imagePath: 'assets/images/drinks/sprite.png',
+                    textTitle: '"Добрый" Sprite',
+                    textWeight: 'Объем: 500мл.',
+                    textPrice: '109 ₽'),
+                Spacer(),
+                ProductCardLong(
+                    imagePath: 'assets/images/drinks/fanta.png',
+                    textTitle: '"Добрый" Fanta',
+                    textWeight: 'Объем: 500мл.',
+                    textPrice: '99 ₽'),
+              ],
+            ),
+            const SizedBox(height: 50),
+            const Row(
+              children: [
+                ProductCardLong(
+                    imagePath: 'assets/images/drinks/cola_033.png',
+                    textTitle: '"Добрый" Cola',
+                    textWeight: 'Объем: 330мл.',
+                    textPrice: '79 ₽'),
+                Spacer(),
+                ProductCardLong(
+                    imagePath: 'assets/images/drinks/bona_gas.png',
+                    textTitle: 'Bona Aqua, газированная',
+                    textWeight: 'Объем: 200мл.',
+                    textPrice: '69 ₽'),
+              ],
+            ),
+            const SizedBox(height: 50),
+            const Row(
+              children: [
+                ProductCardLong(
+                    imagePath: 'assets/images/drinks/bona.png',
+                    textTitle: 'Bona Aqua, негазированная',
+                    textWeight: 'Объем: 200мл.',
+                    textPrice: '69 ₽'),
+                Spacer(),
+                ProductCardLong(
+                    imagePath: 'assets/images/drinks/tea.png',
+                    textTitle: 'Rich, зеленый чай',
+                    textWeight: 'Объем: 500мл.',
+                    textPrice: '109 ₽'),
+              ],
+            ),
+            const SizedBox(height: 50),
+            const Row(
+              children: [
+                ProductCardLong(
+                    imagePath: 'assets/images/drinks/mango.png',
+                    textTitle: 'Rich, зеленый чай с манго',
+                    textWeight: 'Объем: 500мл.',
+                    textPrice: '109 ₽'),
+                Spacer(),
+                ProductCardLong(
+                    imagePath: 'assets/images/drinks/lemon.png',
+                    textTitle: 'Rich, черный чай с лимоном',
+                    textWeight: 'Объем: 500мл.',
+                    textPrice: '109 ₽'),
+              ],
+            ),
+            const SizedBox(height: 50),
+            const Row(
+              children: [
+                ProductCardLong(
+                    imagePath: 'assets/images/drinks/peach.png',
+                    textTitle: 'Rich, черный чай с персиком',
+                    textWeight: 'Объем: 500мл.',
+                    textPrice: '109 ₽'),
+                Spacer(),
+                ProductCardLong(
+                    imagePath: 'assets/images/drinks/apple.png',
+                    textTitle: 'Сок "Добрый", яблоко',
+                    textWeight: 'Объем: 300мл.',
+                    textPrice: '89 ₽'),
+              ],
+            ),
+            const SizedBox(height: 50),
+            const Row(
+              children: [
+                ProductCardLong(
+                    imagePath: 'assets/images/drinks/multifruit.png',
+                    textTitle: 'Сок "Добрый", мультифрукт',
+                    textWeight: 'Объем: 500мл.',
+                    textPrice: '89 ₽'),
+                Spacer(),
+                ProductCardLong(
+                    imagePath: 'assets/images/drinks/orange.png',
+                    textTitle: 'Сок "Добрый", апельсин',
+                    textWeight: 'Объем: 300мл.',
+                    textPrice: '89 ₽'),
+              ],
+            ),
+            const SizedBox(height: 50),
+            const Row(
+              children: [
+                ProductCardLong(
+                    imagePath: 'assets/images/drinks/tomato.png',
+                    textTitle: 'Сок "Добрый", томат',
+                    textWeight: 'Объем: 500мл.',
+                    textPrice: '89 ₽'),
+              ],
+            ),
           ],
         ),
       ),
--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -84,3 +84,174 @@ class ProductCard extends StatelessWidget {
     );
   }
 }
+
+class ProductCardSmall extends StatelessWidget {
+  final String imagePath;
+  final String textTitle;
+  final String? textDescription;
+  final String textPrice;
+  final String? textWeight;
+  const ProductCardSmall({
+    super.key,
+    required this.imagePath,
+    required this.textTitle,
+    this.textDescription,
+    required this.textPrice,
+    this.textWeight,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      height: 350,
+      child: Column(
+        children: [
+          Image.asset(
+            imagePath,
+            height: 140,
+            width: 140,
+            fit: BoxFit.fill,
+          ),
+          const SizedBox(height: 30),
+          Text(textTitle,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w300,
+              )),
+          const SizedBox(height: 15),
+          if (textDescription != null)
+            Text(
+              textDescription!,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w200,
+                height: 1.5,
+              ),
+            ),
+          if (textWeight != null)
+            Text(
+              textWeight!,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w200,
+                height: 1.5,
+              ),
+            ),
+          const Spacer(),
+          Text(
+            textPrice,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: const Color(0xffcc3333), width: 2),
+              borderRadius: const BorderRadius.all(Radius.circular(30)),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 35),
+              child: Text(
+                'В корзину',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Color(0xffcc3333), fontSize: 15),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ProductCardLong extends StatelessWidget {
+  final String imagePath;
+  final String textTitle;
+  final String? textDescription;
+  final String textPrice;
+  final String? textWeight;
+  const ProductCardLong({
+    super.key,
+    required this.imagePath,
+    required this.textTitle,
+    this.textDescription,
+    required this.textPrice,
+    this.textWeight,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      height: 350,
+      child: Column(
+        children: [
+          Image.asset(
+            imagePath,
+            height: 140,
+            width: 140,
+          ),
+          const SizedBox(height: 30),
+          Text(textTitle,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 19.5,
+                fontWeight: FontWeight.w300,
+              )),
+          const SizedBox(height: 15),
+          if (textDescription != null)
+            Text(
+              textDescription!,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w200,
+                height: 1.5,
+              ),
+            ),
+          if (textWeight != null)
+            Text(
+              textWeight!,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w200,
+                height: 1.5,
+              ),
+            ),
+          const Spacer(),
+          Text(
+            textPrice,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: const Color(0xffcc3333), width: 2),
+              borderRadius: const BorderRadius.all(Radius.circular(30)),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 35),
+              child: Text(
+                'В корзину',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Color(0xffcc3333), fontSize: 15),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
